3725b5dcde4955710d3481579445c0ac9942f45e
Sean23189 <front.salat.2000@gmail.com>
2023-07-09 17:40:09 +0200

    Bruh

--- counter.py
+++ counter.py
@@ -1,10 +1,4 @@
 num = 0
-
 while True:
-    num += 1
-
-    # Calculate the number of separators needed based on the length of the number
-    separators = (len(str(num)) - 1) // 3
-
-    formatted_num = "{:,}".format(num)  # Format the number with thousands separators
-    print(f"\r{formatted_num}{' ' * separators}", end="", flush=True)
+        print(f"\r{num}", end = "")
+        num += 1
